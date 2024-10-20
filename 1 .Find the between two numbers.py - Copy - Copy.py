148d64792c886ff2002c9589c73a9b9140f0ee78
faissu321 <ark60541@gmail.com>
2024-10-20 13:10:03 +0530

    Update 1 .Find the between two numbers.py - Copy - Copy.py

--- 1 .Find the between two numbers.py - Copy - Copy.py
+++ 1 .Find the between two numbers.py - Copy - Copy.py
@@ -1,10 +1,7 @@
 a = float(input("Enter first number: "))
 b = float(input("Enter second number: "))
-c = float(input("Enter third number: "))
 
-if a > b and a > c:
+if a > b: 
     print("Maximum is:", a)
-elif b > c:
-    print("Maximum is:", b)
 else:
     print("Maximum is:", c)
